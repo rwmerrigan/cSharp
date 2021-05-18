e2afb683927a7abd79ca9db3366152f2f66c22f7
rmerriga@starbucks.com <rmerriga@starbucks.com>
2021-05-18 13:57:56 -0700

    Finished Chapter 03

---
+++ chapter03/Exercise03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                
+                if(i % 3 ==0 && i % 5 == 0){
+                    System.Console.Write("FizzBuzz, ");
+                }else if(i % 3 == 0 && i % 5 != 0){
+                    System.Console.Write("Fizz, ");
+                }else if(i % 3 != 0 && i % 5 == 0){
+                    System.Console.Write("Buzz, ");
+                }else{
+                    System.Console.Write($"{i}, ");
+                }
+                if(i % 10 == 0){
+                    System.Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Before parsing");
+            Write("What is your age? ");
+            string input = ReadLine();
+            try
+            {
+                int age = int.Parse(input);
+                WriteLine($"You are {age} years old.");
+            }
+            catch (FormatException)
+            {
+                WriteLine("The age you entered is not a valid number format.");
+            }
+            catch (OverflowException)
+            {
+                WriteLine("Age is too big or too small.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            WriteLine("After Parsing");
+        }
+    }
+}
