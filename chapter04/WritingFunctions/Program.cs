43ec0982e6a156b951eeea5986cb43be046dcfa1
rmerriga@starbucks.com <rmerriga@starbucks.com>
2021-05-18 15:46:38 -0700

    Started Chapter 04, moving upstairs

---
+++ chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+
+        static void TimesTable(byte number)
+        {
+            System.Console.WriteLine($"This is the {number} times table:");
+
+            for (int row = 1; row <= 12; row++)
+            {
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
